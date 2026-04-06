4829d8650abd67d929a691cc7eac5052fe5f325d
phanthiennhan230104 <phanthiennhan230104@gmail.com>
2026-04-06 09:16:29 +0700

    refactor: remove legacy template entities

--- src/CleanArchitecture/Domain/Entities/Category.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CleanArchitecture.Domain.Entities;
-
-public class Category
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = null!;
-    public string? Description { get; set; }
-}
